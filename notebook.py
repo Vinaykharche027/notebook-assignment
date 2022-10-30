fe6a42d28e3b98d518db2985190b64407648e175
Vinay Kharche <112177622+Vinaykharche027@users.noreply.github.com>
2022-10-30 13:29:12 +0530

    notebook.py

---
+++ notebook.py
@@ -0,0 +1,31 @@
+# Data types
+''' 
+Data types in python :
+Integer
+Float
+String
+list
+tuple
+Dictionary
+Boolean --> True/false
+
+Examples :->
+'''
+
+a = 2  # integer from numeric
+print("data type of a is : ",type(a))
+
+b = 2.2 # float datatype
+print("data type of b is : ",type(b))
+
+c = "string" #It is a string data type
+print("data type of c is : ",type(c))
+
+d = [1,2,2,3,4,4] #List Datatype
+print("data type of d is : ",type(d))
+
+e = ("Vinay","Kishor","Pavan") #tuple Datatype
+print("data type of e is : ",type(e))
+
+f = {1 : "vinay", 2 : "Kishor",3 : 'Pavan'} #Dictionary Datatype
+print("data type of f is : ",type(f))
